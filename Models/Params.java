06de2f3997fe8bb249b0b84f9547067797fd41b0
SantiNG101 <santinarta101@gmail.com>
2025-08-27 18:48:55 -0300

    Animations go smoother and size parameters for quiver fixed

--- Models/Params.java
+++ Models/Params.java
@@ -2,14 +2,14 @@ package Models;
 
 public class Params {
     int N = 500;                // Número de partículas
-    double L = 40.0;            // Tamaño del espacio cuadrado
-    double v = 0.3;             // Velocidad constante
+    double L = 10.0;            // Tamaño del espacio cuadrado
+    double v = 0.03;             // Velocidad constante
     double eta = 0.1;           // Intensidad del ruido angular (η)
     double r = 1;             // Radio de interacción
-    int steps = 1000;            // Número total de pasos de la simulación
+    int steps = 200;            // Número total de pasos de la simulación
     int saveEvery = 1;          // Cada cuántos pasos se guarda el estado
     String outDir = "outputs";  // Directorio de salida
-    int M = 12;                 // cantidad de celdas por fila/columna
+    int M = 5;                 // cantidad de celdas por fila/columna
     Integer seed = null;
 
     public Params(Double eta, Double v, Double L, Integer N, String outDir) {
